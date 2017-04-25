a92a7fc983f97c750b14aeefed4cd02710f4ae18
Olekciy <0lekciy4@gmail.com>
2017-04-25 09:14:59 +0200

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,26 @@
 import json
+import sys
 
+def read_filepath():
+        filepath = sys.argv[1]
+        return filepath
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r', encoding='UTF-8') as handle:
+        data = json.load(handle)
+    return data
 
 def pretty_print_json(data):
-    pass
-
+    print(json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4))
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = read_filepath()
+        data = load_data(filepath)
+        pretty_print_json(data)
+    except IndexError:
+        print('File path not passed')
+    except FileNotFoundError:
+        print('File not found')
+    except json.JSONDecodeError as err:
+        print('Bad json: {}'.format(err))
