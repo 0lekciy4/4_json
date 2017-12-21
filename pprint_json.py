b177836f20216bc9422ddaa592a540af644cd4cd
Olekciy <0lekciy4@gmail.com>
2017-12-21 13:12:21 +0300

    Update pprint_json.py
    1. Deleted comment
    2. Renamed pretty_print_json as pretty_json
    3. Moved "print (pretty_json (json_content))" from the block "try/except"

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,21 @@
 import json
 import sys
 
-def load_json_content(filepath): #функция load_data переименованна в load_json_content, т.к. лучше подходит по смыслу
+def load_data(filepath):
     with open(filepath, 'r', encoding='UTF-8') as handle:
-        json_content = json.load(handle)
-    return json_content
+        data = json.load(handle)
+    return data
 
-def pretty_print_json(json_content):
+def pretty_json(json_content):
     return json.dumps(json_content, sort_keys=True, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
     try:
-        json_content = load_json_content(sys.argv[1])
-        print(pretty_print_json(json_content))
+        json_content = load_data(sys.argv[1])
     except IndexError:
         print('File path not passed')
     except FileNotFoundError:
         print('File not found')
     except json.JSONDecodeError as err:
         print('Bad json: {}'.format(err))
+    print(pretty_json(json_content))
