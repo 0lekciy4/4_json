6baf041822e96cfe89d4f46828b490f5822e6b60
Olekciy <0lekciy4@gmail.com>
2017-12-22 11:30:40 +0300

    Update pprint_json.py
    1 Renamed "handle" as "json_file"
    2 Renamed "pretty_json" as "return_pretty_json"

--- pprint_json.py
+++ pprint_json.py
@@ -2,11 +2,11 @@ import json
 import sys
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='UTF-8') as handle:
-        json_data = json.load(handle)
+    with open(filepath, 'r', encoding='UTF-8') as json_file:
+        json_data = json.load(json_file)
     return json_data
 
-def pretty_json(json_content):
+def return_pretty_json(json_content):
     return json.dumps(json_content, sort_keys=True, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
@@ -18,4 +18,4 @@ if __name__ == '__main__':
         print('File not found')
     except json.JSONDecodeError as err:
         print('Bad json: {}'.format(err))
-    print(pretty_json(json_content))
+    print(return_pretty_json(json_content))
